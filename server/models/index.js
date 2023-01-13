7701bcd841cad61fb693b4eed547ce5be052e76d
asimcanuz <asimcanuz@gmail.com>
2023-01-14 00:18:20 +0300

    db configuration yapıldı

---
+++ server/models/index.js
@@ -0,0 +1,30 @@
+const dbConfig = require("../config/db.config");
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+  dbConfig.DATABASE,
+  dbConfig.USER,
+  dbConfig.PASSWORD,
+  {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false,
+
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle,
+    },
+  }
+);
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+// import models
+db.users = require("./user.model")(sequelize, Sequelize);
+
+module.exports = db;
