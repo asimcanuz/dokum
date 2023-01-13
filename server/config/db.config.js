005ae3d86c241021a227974cbbd2b208b310218a
asimcanuz <asimcanuz@gmail.com>
2023-01-14 00:18:39 +0300

    db config oluşturuldu

---
+++ server/config/db.config.js
@@ -0,0 +1,16 @@
+// TODO: env.development ve env.production dosyalarını oluştur
+// TODO: proje'nin durumuna .env dosyasına verilerini getir ve onları kullan
+// TODO : REFERENCE : https://nodejs.dev/en/learn/nodejs-the-difference-between-development-and-production/
+module.exports = {
+  HOST: "localhost",
+  DATABASE: "dokum",
+  USER: "root",
+  PASSWORD: "***",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
