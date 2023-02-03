9d94d7d39285318116febe5bec9d447687a96ac9
asimcanuz <asimcanuz@gmail.com>
2023-02-04 00:29:39 +0300

    update description routes

--- server/routes/description.routes.js
+++ server/routes/description.routes.js
@@ -1,4 +1,15 @@
 var express = require("express");
+
+const getDescriptions = require("../controllers/description/getDescriptions");
+const addDescription = require("../controllers/description/addDescription");
+const deleteDescription = require("../controllers/description/deleteDescription");
+const updateDescription = require("../controllers/description/updateDescription");
+
 const router = express.Router();
+
+router.get("/", getDescriptions);
+router.post("/", addDescription);
+router.put("/", updateDescription);
+router.delete("/", deleteDescription);
 
 module.exports = router;
