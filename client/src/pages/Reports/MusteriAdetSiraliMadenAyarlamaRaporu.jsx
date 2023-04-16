9fe70e5a8f3ae19f67f3d7e975bf8d1b0c613478
asimcanuz <asimcanuz@gmail.com>
2023-04-17 01:47:06 +0300

    update

--- client/src/pages/Reports/MusteriAdetSiraliMadenAyarlamaRaporu.jsx
+++ client/src/pages/Reports/MusteriAdetSiraliMadenAyarlamaRaporu.jsx
@@ -255,7 +255,7 @@ const MadenAyarlamaRaporuPDF = ({ trees }) => {
                   <View
                     style={[
                       styles.tableColHeader,
-                      { width: `${100 / mappedHeaders.length}%` },
+                      { width: `${99 / mappedHeaders.length}%` },
                     ]}
                     key={index}
                   >
